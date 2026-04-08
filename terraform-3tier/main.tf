583bf2b7fefb2050ac0fe742732283fe68eef242
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-04-08 22:16:32 +0530

    terraform vpc

---
+++ terraform-3tier/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "aami-0ea87431b78a82070" # Amazon Linux (Mumbai)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-First-Server"
+  }
+}
